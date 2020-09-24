da06da2fba28be7437fb4d62f1c6a64d1a6b0a98
Sabeel <msa_malik@rocketmail.com>
2020-09-24 23:55:22 +0500

    Database added

---
+++ app/src/main/java/com/DriveServiceHelper.java
@@ -0,0 +1,85 @@
+package com;
+
+import androidx.core.util.Pair;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.api.client.http.InputStreamContent;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class DriveServiceHelper {
+    private final Executor mExecutor = Executors.newSingleThreadExecutor();
+    private final Drive mDriveService;
+
+    public DriveServiceHelper(Drive driveService) {
+        mDriveService = driveService;
+    }
+
+
+    public Task<String> uploadFile(String name, java.io.File fileTest) {
+        return Tasks.call(mExecutor, () -> {
+            File metadata = new File()
+                    .setParents(Collections.singletonList("root"))
+                    .setMimeType("text/plain")
+                    .setName(name);
+
+            InputStream targetStream = new FileInputStream(fileTest);
+            InputStreamContent inputStreamContent = new InputStreamContent("text/plain", targetStream);
+            File googleFile = mDriveService.files().create(metadata, inputStreamContent).execute();
+            if (googleFile == null) {
+                throw new IOException("Null result when requesting file creation.");
+            }
+            return googleFile.getId();
+        });
+    }
+
+
+    /**
+     * Opens the file identified by {@code fileId} and returns a {@link Pair} of its name and
+     * contents.
+     */
+    public Task<Pair<String, String>> readFile(String fileId) {
+        return Tasks.call(mExecutor, () -> {
+            // Retrieve the metadata as a File object.
+            File metadata = mDriveService.files().get(fileId).execute();
+            String name = metadata.getName();
+
+            // Stream the file contents to a String.
+            try (InputStream is = mDriveService.files().get(fileId).executeMediaAsInputStream();
+                 BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                String contents = stringBuilder.toString();
+                return Pair.create(name, contents);
+            }
+        });
+    }
+
+    /**
+     * Returns a {@link FileList} containing all the visible files in the user's My Drive.
+     *
+     * <p>The returned list will only contain files visible to this app, i.e. those which were
+     * created by this app. To perform operations on files not created by the app, the project must
+     * request Drive Full Scope in the <a href="https://play.google.com/apps/publish">Google
+     * Developer's Console</a> and be submitted to Google for verification.</p>
+     */
+    public Task<FileList> queryFiles() {
+        return Tasks.call(mExecutor, () ->
+                mDriveService.files().list().setSpaces("drive").execute());
+    }
+
+}
---
+++ app/src/main/java/com/LocalDatabse.java
@@ -0,0 +1,87 @@
+package com;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class LocalDatabse {
+    myDbHelper myhelper;
+
+    public LocalDatabse(Context context) {
+        myhelper = new myDbHelper(context);
+    }
+
+    public long insertData(String DocName, String DocId) {
+        SQLiteDatabase dbb = myhelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(myDbHelper.DOC, DocName);
+        contentValues.put(myDbHelper.DOCID, DocId);
+        long id = dbb.insert(myDbHelper.TABLE_NAME, null, contentValues);
+        return id;
+    }
+
+    public String getDriveData() {
+        SQLiteDatabase db = myhelper.getWritableDatabase();
+        String[] columns = {myDbHelper.SNO, myDbHelper.DOC, myDbHelper.DOCID};
+        Cursor cursor = db.query(myDbHelper.TABLE_NAME, columns, null, null, null, null, null);
+        StringBuffer buffer = new StringBuffer();
+        while (cursor.moveToNext()) {
+            int cid = cursor.getInt(cursor.getColumnIndex(myDbHelper.SNO));
+            String DocName = cursor.getString(cursor.getColumnIndex(myDbHelper.DOC));
+            String DocId = cursor.getString(cursor.getColumnIndex(myDbHelper.DOCID));
+            //   Global.storedSettings.put(type, status);
+            buffer.append(cid + "   " + DocName + "   " + DocId + " \n");
+        }
+        cursor.close();
+        db.close();
+        return buffer.toString();
+    }
+
+    public int delete(String uname) {
+        SQLiteDatabase db = myhelper.getWritableDatabase();
+        String[] whereArgs = {uname};
+
+        int count = db.delete(myDbHelper.TABLE_NAME, myDbHelper.DOC + " = ?", whereArgs);
+        db.close();
+        return count;
+    }
+
+
+    static class myDbHelper extends SQLiteOpenHelper {
+        private static final String DATABASE_NAME = "camscan_india";    // Database Name
+        private static final String TABLE_NAME = "SynDriveRecord";   // Table Name
+        private static final int DATABASE_Version = 1;    // Database Version
+        private static final String SNO = "_id";     // Column I (Primary Key)
+        private static final String DOC = "Doc";    //Column II
+        private static final String DOCID = "DocId";    // Column III
+        private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME +
+                " (" + SNO + " INTEGER PRIMARY KEY AUTOINCREMENT, " + DOC + " VARCHAR(255) ," + DOCID + " VARCHAR(225));";
+        private static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+        private Context context;
+
+        public myDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_Version);
+            this.context = context;
+        }
+
+        public void onCreate(SQLiteDatabase db) {
+            try {
+                db.execSQL(CREATE_TABLE);
+            } catch (Exception e) {
+            }
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            try {
+                db.execSQL(DROP_TABLE);
+                onCreate(db);
+            } catch (Exception e) {
+            }
+        }
+    }
+}
+
